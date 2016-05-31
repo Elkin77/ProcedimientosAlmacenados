9685539f2118ac9c59e503e0394dd4fface55059
Brayan Rojas <brayan0630@gmail.com>
2016-05-31 17:26:34 -0500

    Bosquejo para programar

---
+++ Control.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if(isset($_POST['consola'])){
+    //Se crea el switch para saber que coloco y si no existe lo devuelve con un alert
+    echo '<script>alert("'.$_POST['consola'].'")</script>';
+    header("location: index.php?c=".$_POST['consola']);
+}
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
         <link type="text/css" href="Estilos/EstilosIndex.css" rel="stylesheet">
-        <script type="text/javascript" src="..//js/ajax.js"></script>
+        <!--<script type="text/javascript" src="..//js/ajax.js"></script>-->
         <script>
             function tecla(event) {
 // PARA VER QUE TECLA ESTA PRESIONADO 
@@ -17,15 +17,19 @@ and open the template in the editor.
 
 //VALIDO LA TECLA PRESIONA
                 if (event.keyCode == 13) { //13 = ENTER
-                    alert('hola');
+                    document.getElementById("form1").submit();
                 }
             }
         </script>
     </head>
     <body>
         <div id="web">
-            <form method="post" action="Control.php" name="form1" onkeydown="javascript:tecla(event);">
-                <textarea id="consola" name="consola" cols="175" rows="41"></textarea>
+            <form method="post" action="Control.php" id="form1" name="form1" onkeydown="javascript:tecla(event);">
+                <textarea id="consola" name="consola" cols="175" rows="41"><?php
+                    if(isset($_GET['c'])){
+                    echo $_GET['c'];
+                    }
+                    ?></textarea>
             </form>
         </div>
     </body>
@@ -40,4 +44,5 @@ and open the template in the editor.
 //$datos;
 //while ($row1 = mysql_fetch_row($result)) {
 //    echo '<p>' . $row1[0] . ',' . $row1[1] . ',' . $row1[2] . ',' . $row1[3] . ',' . $row1[4] . ',' . $row1[5] . ',' . $row1[6] . ',' . $row1[7] . '</p>';
-//}?>
+//}
+?>
